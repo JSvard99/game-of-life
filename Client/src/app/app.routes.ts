703a05c50032d2360d2528b083433602447ae15d
Johan Svärd <99josv20@gmail.com>
2026-01-29 00:20:47 +0100

    Add grid to router

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Grid } from './grid/grid';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Grid
+  }
+];
