575b6276afc040c1550ac110ce35cf8149675949
Johan Svärd <99josv20@gmail.com>
2026-01-28 23:43:52 +0100

    Add initialization test for grid

--- Logic.Test/RunnerTest.cs
+++ Logic.Test/RunnerTest.cs
@@ -1,6 +1,13 @@
 ﻿namespace Logic.Test;
 
+[TestFixture]
 public class RunnerTest
 {
-    
+    [Test]
+    public void RunnerCreatesGrid()
+    {
+        var runner = new Runner(10, 5);
+        
+        Assert.That(runner.Grid, Has.Length.EqualTo(50));
+    }
 }
