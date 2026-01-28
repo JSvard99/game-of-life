8093378eef56932483722b6dda5124d47f00f38a
Johan Svärd <99josv20@gmail.com>
2026-01-29 00:09:33 +0100

    Add GET request for grid

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,12 @@
+using Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+var runner = new Runner(10, 5);
+
+app.MapGet("/grid", () => runner.Grid);
 
 app.Run();
