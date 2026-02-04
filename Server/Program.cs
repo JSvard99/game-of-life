a3c54e5939dd16db6c17d5fd1b9e09edc75cd582
Johan Svärd <99josv20@gmail.com>
2026-02-04 16:44:19 +0100

    Enable CORS to server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,16 @@
 using Logic;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        {
+        policy.WithOrigins("*"); 
+        }));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 var runner = new Runner(10, 5);
 
