9b57b2ad7ab16079b45cd4560dc5d233beef7a51
Johan Svärd <99josv20@gmail.com>
2026-02-24 17:07:49 +0100

    Add test for switching cell state

--- GameOfLife.Logic.Test/RunnerTest.cs
+++ GameOfLife.Logic.Test/RunnerTest.cs
@@ -10,4 +10,22 @@ public class RunnerTest
         
         Assert.That(runner.Grid, Has.Length.EqualTo(5));
     }
+
+    [Test]
+    public void SwitchCellState_SwitchesState()
+    {
+        // Arrange
+        const int  row = 3;
+        const int  column = 5;
+        
+        var runner = new Runner(10, 5);
+        var initialState = runner.Grid[row][column];
+        
+        // Act
+        runner.SwitchCellState(row, column);
+        var newState = runner.Grid[row][column];
+        
+        // Assert
+        Assert.That(newState, Is.Not.EqualTo(initialState));
+    }
 }
