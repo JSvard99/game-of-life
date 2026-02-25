c0e506ee5c2f62181306c979ae3e4ad5c3bf6b95
Johan Svärd <99josv20@gmail.com>
2026-02-25 14:31:07 +0100

    Add route to change cell state

--- GameOfLife.Server/Program.cs
+++ GameOfLife.Server/Program.cs
@@ -16,4 +16,17 @@ var runner = new Runner(10, 5);
 
 app.MapGet("/grid", () => runner.Grid);
 
+app.MapPut("/grid/{row}/{column}", (int row, int column, bool state) =>
+{
+    try
+    {
+        runner.SetCellState(row, column, state);
+        return Results.Ok(runner.Grid);
+    }
+    catch (IndexOutOfRangeException e)
+    {
+        return Results.BadRequest("Invalid cell.");
+    }
+});
+
 app.Run();
