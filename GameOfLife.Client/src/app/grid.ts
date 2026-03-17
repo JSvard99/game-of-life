985b763f00f618cddc7f4fa47328c170f1b21e44
Johan Svärd <99josv20@gmail.com>
2026-03-17 14:57:01 +0100

    Move grid logic from component to service

--- GameOfLife.Client/src/app/grid.ts
+++ GameOfLife.Client/src/app/grid.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable, signal, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Grid {
-  
+export class Grid implements OnInit {
+  private http = inject(HttpClient);
+  private API_URL = 'http://localhost:5081';
+  grid = signal<boolean[][]>([]);
+
+  ngOnInit() {
+    this.http.get<Array<Array<boolean>>>(`${this.API_URL}/grid`).subscribe((response) => {
+      this.grid.set(response);
+    });
+  }
+
+  switchCellState(row: number, column: number): void {
+    let newState = !this.grid()[row][column];
+
+    this.http.put<boolean[][]>(`${this.API_URL}/grid/${row}/${column}`, {}, {
+      params: { state: newState },
+    }).subscribe((response) => {
+      this.grid.set(response);
+    })
+  }
 }
--- GameOfLife.Client/src/app/grid/grid.ts
+++ GameOfLife.Client/src/app/grid/grid.ts
@@ -1,5 +1,4 @@
-import {Component, inject, signal, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-grid',
@@ -8,24 +7,8 @@ import {HttpClient} from '@angular/common/http';
   styleUrl: './grid.scss',
 })
 export class Grid implements OnInit {
-  private http = inject(HttpClient);
-  private API_URL = 'http://localhost:5081';
-  grid = signal<boolean[][]>([]);
+    ngOnInit(): void {
 
-  ngOnInit() {
-    this.http.get<Array<Array<boolean>>>(`${this.API_URL}/grid`).subscribe((response) => {
-      this.grid.set(response);
-    });
-  }
-
-  switchCellState(row: number, column: number): void {
-    let newState = !this.grid()[row][column];
-
-    this.http.put<boolean[][]>(`${this.API_URL}/grid/${row}/${column}`, {}, {
-      params: { state: newState },
-    }).subscribe((response) => {
-      this.grid.set(response);
-    })
-  }
+    }
 
 }
