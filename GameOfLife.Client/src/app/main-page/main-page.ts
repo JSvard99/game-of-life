f2f7ef2f98250db74eccc170056bf44ff4eb198a
Johan Svärd <99josv20@gmail.com>
2026-03-28 13:50:57 +0100

    Add control menu to main page

--- GameOfLife.Client/src/app/main-page/main-page.ts
+++ GameOfLife.Client/src/app/main-page/main-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {Grid} from '../grid/grid';
+import {ControlMenu} from '../control-menu/control-menu';
 
 @Component({
   selector: 'app-main-page',
   imports: [
-    Grid
+    Grid,
+    ControlMenu
   ],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
