ff2697ae6e2bebd563fed812e9a052637ac348c8
Johan Svärd <99josv20@gmail.com>
2026-03-28 13:49:16 +0100

    Change root path to main page

--- GameOfLife.Client/src/app/app.routes.ts
+++ GameOfLife.Client/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Grid } from './grid/grid';
+import {MainPage} from './main-page/main-page';
 
 export const routes: Routes = [
   {
     path: "",
-    component: Grid
+    component: MainPage
   }
 ];
